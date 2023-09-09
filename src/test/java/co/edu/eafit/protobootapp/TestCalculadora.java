0730018dbc1806843136163f665a5e8b71ece9b1
leonj <leonj@192.168.1.4>
2023-09-08 21:00:41 -0500

    Now it's ok.

--- src/test/java/co/edu/eafit/protobootapp/TestCalculadora.java
+++ src/test/java/co/edu/eafit/protobootapp/TestCalculadora.java
@@ -6,6 +6,30 @@ import static org.junit.jupiter.api.Assertions.*;
 //Pruebas unitarias del proyecto
 public class TestCalculadora {
 
+    //Pruebas unitarias para el cuadrado
+    @Test
+    public void testCuadradoPositivo() {
+        Calculadora calc = new Calculadora();
+        assertEquals(25.0, calc.cuadrado(5.0), 0);
+    }
+
+    @Test
+    public void testCuadradoCero() {
+        Calculadora calc = new Calculadora();
+        assertEquals(0.0, calc.cuadrado(0.0), 0);
+    }
+
+    @Test
+    public void testCuadradoUno() {
+        Calculadora calc = new Calculadora();
+        assertEquals(1.0, calc.cuadrado(1.0), 0);
+    }
+
+    @Test
+    public void testCuadradoNegativo() {
+        Calculadora calc = new Calculadora();
+        assertEquals(16.0, calc.cuadrado(-4.0), 0);
+    }
     
     //Pruebas unitarias para el cubo
     @Test
