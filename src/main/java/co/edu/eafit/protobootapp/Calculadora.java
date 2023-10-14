5a3ca8d3fea717b8553c9c661b879d66bf52c2c0
Yuly Marcela Velásquez <marcela.velasquez@netuxtecnologia.com>
2023-10-14 08:48:53 -0500

    square and cube are bad

--- src/main/java/co/edu/eafit/protobootapp/Calculadora.java
+++ src/main/java/co/edu/eafit/protobootapp/Calculadora.java
@@ -4,10 +4,10 @@ package co.edu.eafit.protobootapp;
 public class Calculadora {
 
     public Double cuadrado(Double numero) {
-        return numero * numero;
+        return numero + numero;
     }
     
     public Double cubo(Double numero) {
-        return numero * numero * numero;
+        return numero + numero + numero;
     }
 }
